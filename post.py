688a549225ecdcdb802b19035c965b88aa2cbb9e
Andrew Mauney <barca.aj@gmail.com>
2021-06-29 16:44:21 +0000

    initial commit

--- post.py
+++ post.py
@@ -13,10 +13,10 @@ CLIENT_ID = '19c1eccd1f02498faad82917e19d5042'
 CLIENT_SECRET = 'be9c7e5056fc460cb1d2af8d121efce3'
 auth_url = 'https://accounts.spotify.com/api/token'
 
-auth_response = requests.post(auth_url, {
-  'grant_type' : 'client credentials',
-  'client_id' : CLIENT_ID,
-  'client_secret' : CLIENT_SECRET,
-})
+data2 = {'grant_type': 'client_credentials', 'client_id': CLIENT_ID, 'client_secret': CLIENT_SECRET}
+
+
+
+auth_response = requests.post(auth_url, data2)
 
 print(auth_response.status_code)
